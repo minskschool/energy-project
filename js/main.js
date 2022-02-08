f1208b152ab874e13ca146807ad54bb8b4d17417
minskschool <vikameshkova1995@gmail.com>
2022-02-08 22:53:18 +0300

    test

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
-$(window).load(function(){
-	$('#preloader').fadeOut('slow',function(){$(this).remove();});
+$(window).load(function() {
+    $('#preloader').fadeOut('slow', function() { $(this).remove(); });
 });
 
+
+$(function() {
+
+    $('.menuToggle').on('click', function() {
+        $('.main-nav').slideToggle(500, function() {
+            if ($(this).css('display') === 'none') {
+                $(this).removeAttr('style');
+            }
+        });
+        $('.main-nav__link').on('click', function() {
+            $('.main-nav').hide(100, function() {
+                if ($(this).css('display') === 'none') {
+                    $(this).removeAttr('style');
+                }
+            });
+        });
+    });
+});
 
 /******************************************************************************************************************************
 Learn More Page Scroll
@@ -18,47 +36,44 @@ $(function() {
 
 /******************************************************************************************************************************
 Menu
-*******************************************************************************************************************************/ 
+*******************************************************************************************************************************/
 (function() {
 
-	var bodyEl = document.body,
-		//content = document.querySelector( '.content-wrap' ),
-		openbtn = document.getElementById( 'open-button' ),
-		closebtn = document.getElementById( 'close-button' ),
-		isOpen = false;
+    var bodyEl = document.body,
+        //content = document.querySelector( '.content-wrap' ),
+        openbtn = document.getElementById('open-button'),
+        closebtn = document.getElementById('close-button'),
+        isOpen = false;
 
-	function init() {
-		initEvents();
-	}
+    function init() {
+        initEvents();
+    }
 
-	function initEvents() {
-		openbtn.addEventListener( 'click', toggleMenu );
-		if( closebtn ) {
-			closebtn.addEventListener( 'click', toggleMenu );
-		}
+    function initEvents() {
+        openbtn.addEventListener('click', toggleMenu);
+        if (closebtn) {
+            closebtn.addEventListener('click', toggleMenu);
+        }
 
-		/* close the menu element if the target it´s not the menu element or one of its descendants..
-		content.addEventListener( 'click', function(ev) {
-			var target = ev.target;
-			if( isOpen && target !== openbtn ) {
-				toggleMenu();
-			}
-		} );
-		*/
-	}
+        /* close the menu element if the target it´s not the menu element or one of its descendants..
+        content.addEventListener( 'click', function(ev) {
+        	var target = ev.target;
+        	if( isOpen && target !== openbtn ) {
+        		toggleMenu();
+        	}
+        } );
+        */
+    }
 
-	function toggleMenu() {
-		if( isOpen ) {
-			classie.remove( bodyEl, 'show-menu' );
-		}
-		else {
-			classie.add( bodyEl, 'show-menu' );
-		}
-		isOpen = !isOpen;
-	}
+    function toggleMenu() {
+        if (isOpen) {
+            classie.remove(bodyEl, 'show-menu');
+        } else {
+            classie.add(bodyEl, 'show-menu');
+        }
+        isOpen = !isOpen;
+    }
 
-	init();
+    init();
 
 })();
-
-
